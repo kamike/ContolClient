2f052d4dabb1b5d0a0ba98e524648e0ec0fb8ccf
wangtao <wangtao502@hotmail.com>
2017-11-01 16:00:17 +0800

    修改录屏之前

--- app/src/main/java/contol/android/kamike/com/contolclient/MainActivity.java
+++ app/src/main/java/contol/android/kamike/com/contolclient/MainActivity.java
@@ -7,8 +7,13 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
 import android.media.Image;
 import android.media.ImageReader;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
@@ -20,6 +25,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.view.Surface;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -32,16 +38,15 @@ import com.blankj.utilcode.util.ToastUtils;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import contol.android.kamike.com.contolclient.bean.SmsInfoBean;
 import contol.android.kamike.com.contolclient.utils.AllUtils;
 import contol.android.kamike.com.contolclient.utils.PermissionUtil;
-import contol.android.kamike.com.contolclient.utils.SmsUtils;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etIp;
@@ -65,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         mDensity = metrics.densityDpi;
 //        readAllSmsTest();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            bufferInfo = new MediaCodec.BufferInfo();
+        }
 
     }
 
@@ -109,7 +118,9 @@ public class MainActivity extends AppCompatActivity {
     private String SERVER_ADDRESS;
 
     private MediaProjectionManager projectionManager;
-    private int SCREEN_SHOT = 1;
+    private final int SCREEN_SHOT = 1, SCREEN_RECORD = 2;
+
+
     private MediaProjection mediaProject;
     private ImageReader imageReader;
     private int imgWidth = 1080;
@@ -187,9 +198,78 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }, mHandler);
+
+        }
+
+        if (requestCode == SCREEN_RECORD) {
+
+            try {
+                startRecord(resultCode, data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    private void startRecord(int resultCode, Intent data) throws IOException {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+        MediaFormat format = MediaFormat.createVideoFormat("video/avc", imgWidth, imgHeight);
+        //COLOR_FormatSurface这里表明数据将是一个graphicbuffer元数据
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        //设置码率，码率越大视频越清晰，相对的占用内存也要更大
+        format.setInteger(MediaFormat.KEY_BIT_RATE, 6000000);
+        //设置帧数
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        //设置两个关键帧的间隔，这个值你设置成多少对我们这个例子都没啥影响
+        //这个值做视频的朋友可能会懂，反正我不是很懂，大概就是你预览的时候，比如你设置为10，那么你10秒内的预览图都是同一张
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 10);
+
+        //创建一个MediaCodec实例
+        MediaCodec mediaCodec = MediaCodec.createEncoderByType("video/avc");
+        //第一个参数将我们上面设置的format传进去
+        //第二个参数是Surface，如果我们需要读取MediaCodec编码后的数据就要传，但我们这里不需要所以传null
+        //第三个参数关于加解密的，我们不需要，传null
+        //第四个参数是一个确定的标志位，也就是我们现在传的这个
+        mediaCodec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        //获取MediaCodec的surface，这个surface其实就是一个入口，屏幕作为输入源就会进入这个入口，然后交给MediaCodec编码
+        Surface surface = mediaCodec.createInputSurface();
+        mediaCodec.start();
+
+        //第一个参数是输出的地址
+        //第二个参数是输出的格式，我们设置的是mp4格式
+        MediaMuxer mediaMuxer = new MediaMuxer(fileMedia.getAbsolutePath(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+        mediaProject = projectionManager.getMediaProjection(resultCode, data);
+        VirtualDisplay virtualDisplay = mediaProject.createVirtualDisplay( "-display==",
+                imgWidth, imgHeight, mDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                surface, null, null);
+
+        while (!mQuit.get()) {
+            //dequeueOutputBuffer方法你可以这么理解，它会出列一个输出buffer(你可以理解为一帧画面),返回值是这一帧画面的顺序位置(类似于数组的下标)
+            //第二个参数是超时时间，如果超过这个时间了还没成功出列，那么就会跳过这一帧，去出列下一帧，并返回INFO_TRY_AGAIN_LATER标志位
+            int index = mediaCodec.dequeueOutputBuffer(bufferInfo, 10000);
+            //当格式改变的时候吗，我们需要重新设置格式
+            //在本例中，只第一次开始的时候会返回这个值
+            if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                resetOutputFormat(mediaCodec,mediaMuxer);
+
+            } else if (index >= 0) {//这里说明dequeueOutputBuffer执行正常
+                //这里执行我们转换成mp4的逻辑
+                encodeToVideoTrack(index,mediaCodec,mediaMuxer);
+                mediaCodec.releaseOutputBuffer(index, false);
+            }
+        }
+    }
+    private MediaCodec.BufferInfo bufferInfo;
+    private AtomicBoolean mQuit = new AtomicBoolean(false);
+    private File fileMedia;
+    private int videoTrackIndex = -1;
+    /**
+     * 是否在录制视频
+     */
+    private boolean muxerStarted = false;
 
     private void sendBitmaSocket(final Bitmap bitmap) {
         if (bitmap == null) {
@@ -296,13 +376,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void readAllSmsTest() {
-        ArrayList<SmsInfoBean> list = SmsUtils.getAllSMS();
-        LogUtils.i("读取到了多少条短信：" + list.size());
-        for (SmsInfoBean sms : list) {
-            LogUtils.i("" + sms.toString());
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void encodeToVideoTrack(int index, MediaCodec mediaCodec,MediaMuxer mediaMuxer) {
+        //通过index获取到ByteBuffer(可以理解为一帧)
+        ByteBuffer encodedData = mediaCodec.getOutputBuffer(index);
+        //当bufferInfo返回这个标志位时，就说明已经传完数据了，我们将bufferInfo.size设为0，准备将其回收
+        if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            bufferInfo.size = 0;
+        }
+        if (bufferInfo.size == 0) {
+            encodedData = null;
+        }
+        if (encodedData != null) {
+            encodedData.position(bufferInfo.offset);//设置我们该从哪个位置读取数据
+            encodedData.limit(bufferInfo.offset + bufferInfo.size);//设置我们该读多少数据
+            //这里将数据写入
+            //第一个参数是每一帧画面要放置的顺序
+            //第二个是要写入的数据
+            //第三个参数是bufferInfo，这个数据包含的是encodedData的offset和size
+            mediaMuxer.writeSampleData(videoTrackIndex, encodedData, bufferInfo);
+
         }
     }
 
+    //这个方法其实就是设置MediaMuxer的Format
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void resetOutputFormat(MediaCodec mediaCodec, MediaMuxer mediaMuxer) {
+        //将MediaCodec的Format设置给MediaMuxer
+        MediaFormat newFormat = mediaCodec.getOutputFormat();
+        //获取videoTrackIndex，这个值是每一帧画面要放置的顺序
+        videoTrackIndex = mediaMuxer.addTrack(newFormat);
+        mediaMuxer.start();
+        muxerStarted = true;
+    }
 
+
+    public void onclickRecord(View view) {
+
+    }
 }
